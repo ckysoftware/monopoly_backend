f983dd7693e661f9ca63f892cab6c93462bc1bff
ckysoftware <leckysoftware@gmail.com>
2022-05-01 23:46:24 +0800

    break down test_player

--- test/test_player/test_player.py
+++ test/test_player/test_player.py
@@ -1,12 +1,14 @@
 import pytest
 
 from player import Player
-from constants import CONST_STARTING_CASH, CONST_GOAL_MONEY
+from constants import CONST_STARTING_CASH, CONST_GO_MONEY
+
 
 @pytest.fixture
 def player_simple():
     player = Player(name="Player 1", character=0)
     return player
+
 
 @pytest.fixture
 def player_comp():
@@ -18,6 +20,7 @@ def player_comp():
         position=10
     )
     return player
+
 
 def test_player_init(player_simple, player_comp):
     assert player_simple.name == 'Player 1'
@@ -33,31 +36,30 @@ def test_player_init(player_simple, player_comp):
     assert player_comp.position == 10
 
 
-def test_move_player(player_simple, player_comp):
+def test_move_player_from_zero(player_simple):
     assert player_simple.move(10) == 10
     assert player_simple.position == 10
 
+
+def test_move_player_from_nonzero(player_comp):
     assert player_comp.move(10) == 20
     assert player_comp.position == 20
 
 
-def test_reset_player_position(player_simple, player_comp):
+def test_reset_player_position_from_zero(player_simple):
     player_simple.reset_position()
     assert player_simple.position == 0
-    assert player_simple.cash.balance == CONST_STARTING_CASH + CONST_GOAL_MONEY
+    assert player_simple.cash.balance == CONST_STARTING_CASH + CONST_GO_MONEY
 
+
+def test_reset_player_position_from_nonzero(player_comp):
     player_comp.reset_position()
     assert player_comp.position == 0
-    assert player_comp.cash.balance == 3000 + CONST_GOAL_MONEY
+    assert player_comp.cash.balance == 3000 + CONST_GO_MONEY
 
 
-def test_reset_player_position_and_move(player_simple, player_comp):
-    player_simple.reset_position()
-    player_simple.move(10)
-    assert player_simple.position == 10
-    assert player_simple.cash.balance == CONST_STARTING_CASH + CONST_GOAL_MONEY
-
+def test_reset_player_position_and_move(player_comp):
     player_comp.reset_position()
     player_comp.move(10)
     assert player_comp.position == 10
-    assert player_comp.cash.balance == 3000 + CONST_GOAL_MONEY
+    assert player_comp.cash.balance == 3000 + CONST_GO_MONEY
