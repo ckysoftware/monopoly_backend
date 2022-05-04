6c8364b7066da0596a955d9211b85eacff4377e9
ckysoftware <leckysoftware@gmail.com>
2022-05-05 03:26:06 +0800

    added utility card

---
+++ src/utility_card.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from src.actions import Action as a
+from src.property import Property
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:  # Only imports the below statements during type checking
+    from src.player import Player
+
+
+@dataclass(kw_only=True)
+class UtilityCard(Property):
+
+    def compute_rent(self, dice_count) -> int:
+        if self.mortgaged:
+            return 0
+        elif self.property_set.monopoly:
+            return dice_count * 10
+        else:
+            return dice_count * 4
+
+    def mortgage(self) -> None:
+        if self.mortgaged:
+            raise ValueError("Property is already mortgaged")
+        elif self.owner_character is None:
+            raise ValueError("Property has no owner")
+        self.mortgaged = True
+
+    # TODO unmortgage
+    def unmortgage(self) -> None:
+        pass
+
+    def trigger(self, player: Player) -> int:
+        if self.owner_character is None:
+            return a.ASK_TO_BUY
+        elif self.owner_character == player.character:
+            return a.NOTHING
+        elif self.owner_character != player.character:
+            return a.CHARGE_RENT
+        else:
+            raise ValueError("Unknown action")
---
+++ test/test_utility_card/test_utility_card.py
@@ -0,0 +1,129 @@
+import pytest
+
+from src.utility_card import UtilityCard
+from src.property_set import PropertySet
+from src.player import Player
+
+from src.actions import Action as a
+
+
+@pytest.fixture
+def util_card_simple():
+    property_set = PropertySet(set_id=0)
+    utility_card = UtilityCard(
+        name="Utility 1",
+        price=150,
+        property_set=property_set,
+    )
+    property_set.add_property(utility_card)
+    return utility_card
+
+
+@pytest.fixture
+def util_card_monopoly():
+    property_set = PropertySet(set_id=0)
+    utility_card = UtilityCard(
+        name="Utility 1",
+        price=150,
+        property_set=property_set,
+    )
+    property_set.add_property(utility_card)
+    property_set.monopoly = True
+    return utility_card
+
+
+@pytest.fixture
+def util_card_diff_owners():
+    property_set = PropertySet(set_id=0)
+    util_card_1 = UtilityCard(
+        name="Utility 1",
+        price=150,
+        property_set=property_set,
+        owner_character=1,
+    )
+    util_card_2 = UtilityCard(
+        name="Utility 2",
+        price=150,
+        property_set=property_set,
+        owner_character=10,
+    )
+    property_set.add_property(util_card_1)
+    property_set.add_property(util_card_2)
+    return util_card_1
+
+
+@pytest.fixture
+def player_simple():
+    player = Player(name="Player 1", character=0)
+    return player
+
+
+def test_utility_card_init(util_card_simple):
+    assert util_card_simple.name == 'Utility 1'
+    assert util_card_simple.price == 150
+    assert util_card_simple.property_set.set_id == 0
+    assert id(util_card_simple.property_set.properties[0]) == id(util_card_simple)
+    assert util_card_simple.mortgaged is False
+    assert util_card_simple.owner_character is None
+
+
+def test_assign_owner(util_card_simple):
+    util_card_simple.assign_owner(2)
+    assert util_card_simple.owner_character == 2
+
+
+def test_assign_owner_not_monopoly(util_card_diff_owners):
+    util_card_diff_owners.assign_owner(2)
+    assert util_card_diff_owners.property_set.monopoly is False
+
+
+def test_assign_owner_is_monopoly(util_card_diff_owners):
+    util_card_diff_owners.assign_owner(10)
+    assert util_card_diff_owners.property_set.monopoly is True
+
+
+def test_compute_rent_without_monopoly(util_card_simple):
+    assert util_card_simple.compute_rent(dice_count=5) == 20
+
+
+def test_compute_rent_with_monopoly(util_card_monopoly):
+    assert util_card_monopoly.compute_rent(dice_count=10) == 100
+
+
+def test_compute_rent_mortgaged_without_monopoly(util_card_simple):
+    util_card_simple.mortgaged = True
+    assert util_card_simple.compute_rent(dice_count=10) == 0
+
+
+def test_compute_rent_mortgaged_with_monopoly(util_card_monopoly):
+    util_card_monopoly.mortgaged = True
+    assert util_card_monopoly.compute_rent(dice_count=5) == 0
+
+
+def test_mortgage(util_card_diff_owners):
+    util_card_diff_owners.mortgage()
+    assert util_card_diff_owners.mortgaged is True
+
+
+def test_mortgage_again(util_card_diff_owners):
+    util_card_diff_owners.mortgage()
+    with pytest.raises(ValueError, match='Property is already mortgaged'):
+        util_card_diff_owners.mortgage()
+
+
+def test_mortgage_no_owner(util_card_simple):
+    with pytest.raises(ValueError, match="Property has no owner"):
+        util_card_simple.mortgage()
+
+
+def test_trigger_unowned(util_card_simple, player_simple):
+    assert util_card_simple.trigger(player_simple) == a.ASK_TO_BUY
+
+
+def test_trigger_diff_owner(util_card_diff_owners, player_simple):
+    assert util_card_diff_owners.trigger(player_simple) == a.CHARGE_RENT
+
+
+def test_trigger_same_owner(util_card_diff_owners, player_simple):
+    player_simple.character = 1
+    assert util_card_diff_owners.trigger(player_simple) == a.NOTHING
