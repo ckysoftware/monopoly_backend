fbb9ba1e21e15714b3afc0d0323642f2f9fa2ba4
ckysoftware <leckysoftware@gmail.com>
2022-05-06 04:14:20 +0800

    added dice

---
+++ src/game/dice.py
@@ -0,0 +1,17 @@
+"""
+However, if a player rolls Doubles three times in succession (in one turn),
+they immediately move to Jail without moving the third time. The only exception
+on this is if the player threw 2 doubles,
+then hit a nearest utility space from the "Chance" space that is not mortgaged
+or unsold, and then the player hits a third pair of doubles,
+which would normally send them to jail. However since they only did it for the rent calculation and not movement,
+playing continues unless they hit Doubles a fourth time in a row, thus going to jail.
+"""
+
+import random
+
+
+def roll(num_faces: int = 6, num_dice: int = 2) -> (int):
+    return (random.randint(1, num_faces) for _die in range(num_dice))
+
+    # TODO handle 3 double roll, handle double due to utility calculation, probably do it in game logic
---
+++ test/test_dice/test_dice.py
@@ -0,0 +1,8 @@
+import src.game.dice as dice
+
+
+def test_roll_dice():
+    for _ in range(100):
+        dice_1, dice_2 = dice.roll(num_faces=6, num_dice=2)
+        assert dice_1 in list(range(1, 7))
+        assert dice_2 in list(range(1, 7))
