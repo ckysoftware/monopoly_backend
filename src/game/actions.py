6c67717507439f46ad5e6eea96eefb75048ede16
ckysoftware <leckysoftware@gmail.com>
2022-05-07 01:54:27 +0800

    added constants and actions

--- src/game/actions.py
+++ src/game/actions.py
@@ -9,3 +9,11 @@ class Action(Enum):
     ASK_TO_BUY = auto()  # land on property card and ask if player wants to buy it
     START_AUCTION = auto()  # start the auction for property card
     CHARGE_RENT = auto()  # the player landed needs to pay rent
+
+    # movement
+    ASK_TO_ROLL = auto()
+
+    # event
+    SEND_TO_JAIL = auto()
+
+    PASS_GO = auto()
