e32571991c3c5b170ca872a6cb5f61ed8c85b1ef
ckysoftware <leckysoftware@gmail.com>
2022-05-07 01:54:44 +0800

    added map size

--- src/game/game_map.py
+++ src/game/game_map.py
@@ -4,13 +4,17 @@ from src.game.place.place import Place
 from src.game.player import Player
 
 
-@dataclass
+@dataclass(kw_only=True)
 class GameMap:
     """
     This class represents the map of the game.
     """
 
     map_list: list[Place] = field(default_factory=list)
+    size: int = None
+
+    def __post_init__(self):
+        self.size = len(self.map_list)
 
     def trigger(self, player: Player) -> int:
         """
